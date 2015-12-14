9e3f1a81cc11f42018e697606f08546aebf9a496
Helene <helene@outlook.com>
2015-12-14 15:54:14 -0700

    now with google map

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
 var ajaxCall = $.ajax( {
-  url: 
+  url:
   method: "GET",
   dataType: "json"
 });
+
+function initialize() {
+       var mapCanvas = document.getElementById('map');
+       var mapOptions = {
+         center: new google.maps.LatLng(44.5403, -78.5463),
+         zoom: 8,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+       }
+       var map = new google.maps.Map(mapCanvas, mapOptions)
+     };
+     google.maps.event.addDomListener(window, 'load', initialize);
