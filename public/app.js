7219e4bc684e0430280b25f5c4afecae7f7506f0
Helene <helene@outlook.com>
2015-12-16 11:36:03 -0700

    cors-anywhere test

--- public/app.js
+++ public/app.js
@@ -1,9 +1,25 @@
 
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
 
 jQuery(document).ready(function() {
   $.ajax({
 
-    url : "https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/d491a9e3880f0a90/forecast/q/80202.json",
+    url : "http://api.wunderground.com/api/d491a9e3880f0a90/forecast/q/80202.json",
     method: "GET",
     dataType: "jsonp",
     success: function(parsed_json){
@@ -22,8 +38,8 @@ jQuery(document).ready(function() {
     }
   });
   $.ajax( {
-    url:"https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/d491a9e3880f0a90/conditions/q/80202.json",
-    method: "GET",
+    url:"http://api.wunderground.com/api/d491a9e3880f0a90/conditions/q/80202.json",
+      method: "GET",
     dataType: "jsonp",
     success : function(response) {
       var currentLocation = response["current_observation"]["display_location"]["full"];
